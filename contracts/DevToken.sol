6d3fc687acb42cf0d2049845913786730c2a94cb
andrewhuang2019 <andrewhuang2019@gmail.com>
2024-07-08 15:57:25 -0500

    Updated DevToken
    Updated DevToken with new constructor and ownership permissions

--- contracts/DevToken.sol
+++ contracts/DevToken.sol
@@ -4,10 +4,18 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DevToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Developer", "DEV") {
-        _mint(msg.sender, initialSupply);
+
+contract DevToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner) 
+    ERC20("Developer", "DEV") 
+    Ownable(initialOwner)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
     function decmials() public view virtual returns (uint8) {
