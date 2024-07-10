0d103f24de5df9c4c1d90b41b1da80c47b86a5b2
andrewhuang2019 <andrewhuang2019@gmail.com>
2024-07-10 17:16:45 -0500

    Updated front-back-migration
    Updated front-back-migration for implementing react and hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-ethers")
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
+};
