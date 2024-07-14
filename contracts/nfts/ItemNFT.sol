404857545544cd47cf645b2ae15fd0811af38a52
andrewhuang2019 <andrewhuang2019@gmail.com>
2024-07-13 21:09:02 -0500

    Create ItemNFT.sol
    Created NFT file for the ItemNFTs. Will test out the deployment later.

---
+++ contracts/nfts/ItemNFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ItemNFT is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("ItemNFT", "INT")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
