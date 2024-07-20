b842910e8ba9420776689deab5a18b19d1e59778
andrewhuang2019 <andrewhuang2019@gmail.com>
2024-07-20 17:06:11 -0500

    Update ItemForm.jsx

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -35,7 +35,7 @@ const ItemForm = () => {
     const handleSubmit = async (values, actions) => {
         if (file) {
             const reader = new FileReader();
-            reader.onloadend = () => {
+            reader.onloadend = async () => {
                 const base64Image = reader.result;
 
                 const updatedData = {
@@ -52,8 +52,31 @@ const ItemForm = () => {
                     image: base64Image
                 };
 
-
+                
                 //Try to send the data to IPFS system and get the CID back from them. 
+                try {
+                    const formData = new FormData();
+                    formData.append("file", file);
+    
+                    const response = await fetch(
+                        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                        {
+                            method: "POST",
+                            headers: {
+                                Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+                            },
+                            body: formData
+                        },
+    
+                    );
+
+                    if (response.ok){
+                        const data = await response.json();
+                    }
+
+                } catch (error) {
+
+                }
 
 
                 setTimeout(() =>{
